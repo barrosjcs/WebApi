14cac63a8c0fa409f76b5eb926dd211625fe1cc4
Jefferson <je_caetano@hotmail.com>
2017-07-17 23:17:14 -0300

    Inicio

--- WebApi/App_Start/WebApiConfig.cs
+++ WebApi/App_Start/WebApiConfig.cs
@@ -14,6 +14,7 @@ namespace WebApi
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            // Rota no WebApi é api/{controller}
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
