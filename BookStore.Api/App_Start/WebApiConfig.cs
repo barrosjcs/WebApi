275a578b6d9b67e977817e9722f7e02be9430d6e
Jefferson <je_caetano@hotmail.com>
2017-08-13 18:25:08 -0300

    Modificar a serialização

--- BookStore.Api/App_Start/WebApiConfig.cs
+++ BookStore.Api/App_Start/WebApiConfig.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Http;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Web.Http;
 
 namespace BookStore.Api
 {
@@ -11,6 +13,11 @@ namespace BookStore.Api
             // Remove o XML Formatter. XML é mais pesado, então para economizar o trafego de dados é melhor remove-lo e utilizar JSON
             var formatters = GlobalConfiguration.Configuration.Formatters;
             formatters.Remove(formatters.XmlFormatter);
+
+            // Modificar a serialização. Passar json iniciando em minusculo
+            var jsonSettings = formatters.JsonFormatter.SerializerSettings;
+            jsonSettings.Formatting = Formatting.Indented;
+            jsonSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
